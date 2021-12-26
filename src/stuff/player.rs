c1598fc49f41e6c690b3422997c7afeac65446ba
Ryan Blecher <notryanb@gmail.com>
2021-12-26 11:19:45 -0500

    Refactor the player into it's own module, add double clicking, change behavior of the player UI to match foobar2000, make a stuff module because I'm unsure of how to organize the app

---
+++ src/stuff/player.rs
@@ -0,0 +1,92 @@
+use crate::stuff::playlist::Track;
+
+pub struct Player {
+    pub track_state: TrackState,
+    pub selected_track: Option<Track>,
+    pub sink: rodio::Sink,
+    pub stream_handle: rodio::OutputStreamHandle,
+}
+
+impl Player {
+    pub fn new(sink: rodio::Sink, stream_handle: rodio::OutputStreamHandle) -> Self {
+        Self {
+            track_state: TrackState::Unstarted,
+            selected_track: None,
+            sink: sink,
+            stream_handle: stream_handle,
+        }
+    }
+
+    pub fn stop(&mut self) {
+        match &self.track_state {
+            TrackState::Playing | TrackState::Paused => {
+                self.track_state = TrackState::Stopped;
+                self.sink.stop();
+            }
+            _ => (),
+        }
+    }
+
+    pub fn play(&mut self) {
+        if let Some(selected_track) = &self.selected_track {
+            let file = std::io::BufReader::new(
+                std::fs::File::open(&selected_track.path).expect("Failed to open file"),
+            );
+            let source = rodio::Decoder::new(file).expect("Failed to decode audio file");
+
+            match self.track_state {
+                TrackState::Unstarted | TrackState::Stopped | TrackState::Playing => {
+                    self.track_state = TrackState::Playing;
+
+                    let sink_try = rodio::Sink::try_new(&self.stream_handle);
+
+                    match sink_try {
+                        Ok(sink) => {
+                            self.sink = sink;
+                            self.sink.append(source);
+                        }
+                        Err(e) => tracing::error!("{:?}", e),
+                    }
+                }
+                TrackState::Paused => {
+                    self.track_state = TrackState::Playing;
+                    self.sink.play();
+                }
+                _ => (),
+            }
+        }
+    }
+
+    // Toggle pause between paused and playing.
+    pub fn pause(&mut self) {
+        match self.track_state {
+            TrackState::Playing => {
+                self.track_state = TrackState::Paused;
+                self.sink.pause();
+            }
+            TrackState::Paused => {
+                self.track_state = TrackState::Playing;
+                self.sink.play();
+            }
+            _ => (),
+        }
+    }
+}
+
+pub enum TrackState {
+    Unstarted,
+    Stopped,
+    Playing,
+    Paused,
+}
+
+impl std::fmt::Display for TrackState {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            TrackState::Unstarted => write!(f, "Unstarted"),
+            TrackState::Stopped => write!(f, "Stopped"),
+            TrackState::Playing => write!(f, "Playing"),
+            TrackState::Paused => write!(f, "Paused"),
+        }
+    }
+}
