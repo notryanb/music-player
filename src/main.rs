1f4f787b716cdbd10400b04e01cddc11da4cafb9
Ryan Blecher <notryanb@gmail.com>
2023-03-04 18:20:30 -0500

    Change to using fft sync resampling. Seems to be a lot faster

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use cpal::SampleFormat;
 use eframe::egui;
 use minimp3::{Decoder, Frame};
-use rubato::{InterpolationParameters, InterpolationType, Resampler, SincFixedIn, WindowFunction};
+use rubato::Resampler;
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{Read, Seek};
+use std::io::{Read, Seek, Write};
 use std::sync::atomic::{AtomicU32, Ordering::*};
 use std::sync::mpsc::channel;
 use std::sync::Arc;
@@ -18,7 +18,6 @@ use std::thread;
 
 mod app;
 
-// This holds several buffers
 pub struct AudioBank {
     pub buffers: HashMap<usize, AudioBuffer>,
     pub count: usize, // This can be replaced if we randomize the key for the map
@@ -152,6 +151,36 @@ where
         let frame_value = self.current_frame.data[self.current_frame_offset];
         self.current_frame_offset += 1;
         Some(frame_value)
+    }
+}
+
+
+fn read_frames<R: Read + Seek>(input_buffer: &mut R, nbr: usize, channels: usize) -> Vec<Vec<f32>> {
+    let mut buffer = vec![0u8; 4];
+    let mut wfs = Vec::with_capacity(channels);
+    for _chan in 0..channels {
+       wfs.push(Vec::with_capacity(nbr)); 
+    }
+    let mut value: f32;
+    for _frame in 0..nbr {
+        for wf in wfs.iter_mut().take(channels) {
+            input_buffer.read(&mut buffer).unwrap();
+            value = f32::from_le_bytes(buffer.as_slice().try_into().unwrap()) as f32;
+            wf.push(value);
+        }
+    }
+
+    wfs
+}
+
+fn write_frames<W: Write + Seek>(waves: Vec<Vec<f32>>, output_buffer: &mut W, channels: usize) {
+    let nbr = waves[0].len();
+    for frame in 0..nbr {
+        for chan in 0..channels {
+            let value = waves[chan][frame];
+            let bytes = value.to_le_bytes();
+            output_buffer.write(&bytes).unwrap();
+        }
     }
 }
 
@@ -324,68 +353,47 @@ fn main() {
                                     *guard = sample_rate; 
                                 }
 
-                                let mut the_samples = mp3_decoder.collect::<Vec<i16>>();
+                                let start = std::time::Instant::now();
+                                let raw_samples = mp3_decoder
+                                    .flat_map(|sample| (sample as f32).to_le_bytes())
+                                    .collect::<Vec<u8>>();
 
-                                if sample_rate != device_sample_rate {
-                                    tracing::info!("Resampling from {sample_rate} to {device_sample_rate}");
-                                    let mut left = vec![];
-                                    let mut right = vec![];
-                                    for (idx, sample) in the_samples.iter().enumerate() {
-                                        if idx % 2 == 0 {
-                                            left.push(*sample as f32);
-                                        } else {
-                                            right.push(*sample as f32);
-                                        }
+                                let mut input_cursor = std::io::Cursor::new(&raw_samples);
+                                let capacity = (*(&raw_samples.len()) as f32 * (device_sample_rate as f32 / sample_rate as f32)) as usize;
+                                let mut output_buffer = Vec::with_capacity(capacity);
+                                let mut output_cursor = std::io::Cursor::new(&mut output_buffer);
+                                let channels = 2;
+                                let chunk_size = 1024;
+                                let sub_chunks = 2;
+
+                                let mut fft_resampler = rubato::FftFixedIn::<f32>::new(
+                                    44100 as usize,
+                                    48000 as usize,
+                                    chunk_size,
+                                    sub_chunks,
+                                    channels
+                                ).unwrap();
+
+                                let num_frames_per_channel = fft_resampler.input_frames_next();
+                                let sample_byte_size = 8;
+                                let num_chunks = &raw_samples.len() / (sample_byte_size * channels * num_frames_per_channel);
+
+                                for _chunk in 0..num_chunks {
+                                    let frame_data = read_frames(&mut input_cursor, num_frames_per_channel, channels);
+                                    let output = fft_resampler.process(&frame_data, None).unwrap();
+                                    write_frames(output, &mut output_cursor, channels);
+                                }
+
+                                let end = start.elapsed();
+                                tracing::info!("Done resampling file: {:?}ms", end);
+
+
+                                for bytes in output_buffer.iter().as_slice().chunks(4) {
+                                    let sample = (f32::from_le_bytes([bytes[0], bytes[1], bytes[2], bytes[3]])) as i16;
+                                    {
+                                        let mut guard = ab.lock().unwrap();
+                                        guard.append_sample(key, sample);
                                     }
-
-                                    assert!(left.len() == right.len());
-
-                                    let params = InterpolationParameters {
-                                        sinc_len: 256,
-                                        f_cutoff: 0.95,
-                                        interpolation: InterpolationType::Linear,
-                                        oversampling_factor: 256,
-                                        window: WindowFunction::BlackmanHarris2,
-                                    };
-
-                                    let mut resampler = SincFixedIn::<f32>::new(
-                                        48_000 as f64 / 44_100 as f64, // should be using config_sample_rate / the mp3's sample_rate
-                                        2.0,
-                                        params,
-                                        left.len(),
-                                        2,
-                                    )
-                                    .unwrap();
-
-                                    let channels = vec![left, right];
-
-                                    let resampled_audio = resampler
-                                        .process(&channels, None)
-                                        .expect("failed to resample audio");
-
-                                    let zip_audio = resampled_audio[0]
-                                        .iter()
-                                        .zip(&resampled_audio[1])
-                                        .collect::<Vec<(&f32, &f32)>>();
-                                        //.flat_map(|z| vec![*z.0 as i16, *z.1 as i16])
-                                        //.collect::<Vec<i16>>();
-
-                                    let mut vec_channels = vec![];
-                                    for z in zip_audio {
-                                        vec_channels.push(vec![*z.0, *z.1]);
-                                    }
-
-                                    the_samples = vec_channels
-                                        .iter()
-                                        .flatten()
-                                        .map(|s| *s as i16)
-                                        .collect::<Vec<i16>>();
-                                } else { tracing::info!("no resampling needed"); }
-
-                                // append resampled data to audio bank buffer
-                                {
-                                    let mut guard = ab.lock().unwrap();
-                                    guard.append_samples(key, the_samples);
                                 }
 
                                 tracing::info!("Done loading file");
